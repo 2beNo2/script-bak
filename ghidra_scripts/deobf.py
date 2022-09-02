0d96b77731a167ae8f47849d8d31b2ee48d13a08
huangchen1992 <hc_0117@qq.com>
2022-09-02 18:40:50 +0800

    update ghidra_scripts

--- ghidra_scripts/deobf.py
+++ ghidra_scripts/deobf.py
@@ -1,17 +1,19 @@
-#coding=utf-8
-
-
-#@category    Analysis.deobf
-
-
+# coding=utf-8
+#@category OLLVM
 
 import os
 import binascii
 import logging
 
+try:
+    from ghidra.ghidra_builtins import *
+except:
+    pass
+
+
+from ghidra.program.model.pcode import PcodeOp
 from ghidra.app.decompiler import DecompInterface
 from ghidra.program.model.mem import *
-from ghidra.program.model.pcode import PcodeOp
 from ghidra.app.plugin.assembler import Assemblers
 
 logging.basicConfig(level=logging.INFO,
@@ -21,31 +23,58 @@ logging.basicConfig(level=logging.INFO,
 
 # 获取块中最后一条pcode
 def get_last_pcode(block):
+    pcode = None
     pcode_iterator = block.getIterator()
     while pcode_iterator.hasNext():
         pcode = pcode_iterator.next()
-        if not pcode_iterator.hasNext():
-            return pcode
+    return pcode
 
 
 # 获取真实块
 def get_real_block(blocks):
     for block in blocks:
-        pcode =  get_last_pcode(block)
-        if pcode.getOpcode() == PcodeOp.BRANCH:
-            logging.info("block = %s, Parent = %s" %(block.toString(), block.getIn(0).toString()))
+        pCode = get_last_pcode(block)
+        if pCode.getOpcode() == PcodeOp.BRANCH:
+            logging.info("block = %s, Parent = %s" % (block.toString(), block.getIn(0).toString()))
 
+
+def get_state_var(high_function, current_address):
+    pcode_iterator = high_function.getPcodeOps(current_address)
+    pcode = None
+
+    # find the pcode for COPYing const
+    while pcode_iterator.hasNext():
+        pcode = pcode_iterator.next()
+        logging.debug('finding COPY const pcode: %s' % pcode)
+        if pcode.getOpcode() == PcodeOp.COPY and pcode.getInput(0).isConstant():
+            break
+
+    logging.info('COPY const pcode: %s' % pcode)
+
+    # find the state var in phi node
+    depth = 0
+    while pcode is not None and pcode.getOpcode() != PcodeOp.MULTIEQUAL:
+        logging.debug('finding phi node: %s, depth %d' % (pcode, depth))
+        if pcode.getOutput() is None:
+            logging.warning('output is None in %s' % pcode)
+            break
+        pcode = pcode.getOutput().getLoneDescend()
+        if depth > 5:
+            break
+        depth += 1
+
+    if pcode is None or pcode.getOpcode() != PcodeOp.MULTIEQUAL:
+        logging.error('cannot find phi node')
+        return None
+    else:
+        logging.info('phi node: %s' % pcode)
+
+    state_var = pcode.getOutput()
+    logging.info('state var is %s' % state_var)
+    return state_var
 
 
 if __name__ == '__main__':
-    # init
-    decomplib = DecompInterface()
-    decomplib.openProgram(currentProgram)
-
-    current_function = getFunctionContaining(currentAddress)
-    decompile_res = decomplib.decompileFunction(current_function, 1200, getMonitor())
-    high_function = decompile_res.getHighFunction()
-
     '''
     1、函数的开始地址为序言（Prologue）的地址
     2、序言的后继为主分发器（Main dispatcher）
@@ -55,17 +84,69 @@ if __name__ == '__main__':
     6、剩下的为无用块与子分发器（Sub dispatchers）
     '''
 
-    # 获取序言块和主分发器
+    # init
+    decompLib = DecompInterface()
+    decompLib.openProgram(currentProgram)
+
+    current_function = getFunctionContaining(currentAddress)
+    decompile_res = decompLib.decompileFunction(current_function, 1200, getMonitor())
+    high_function = decompile_res.getHighFunction()
+    if high_function is None:
+        logging.info("decompileFunction error : %s" % decompile_res.getErrorMessage())
+        exit()
+    else:
+        logging.info("currentProgram : %s" % currentProgram.toString())
+        logging.info("currentAddress : %s" % currentAddress.toString())
+        logging.info("current_function : %s" % current_function.toString())
+        logging.info("decompile_res : %s" % decompile_res.toString())
+        logging.info("high_function : %s" % high_function.toString())
+
+    '''
+    获取序言块和主分发器
+    '''
     blocks = high_function.getBasicBlocks()
     prologue_block = blocks[0]
-    dispatcher_block = blocks[1]
+    main_dispatcher_block = blocks[1]
     logging.info("prologue_block : %s" % prologue_block.toString())
-    logging.info("dispatcher_block : %s" % dispatcher_block.toString())
+    logging.info("main_dispatcher_block : %s" % main_dispatcher_block.toString())
 
-    for i in range(2, len(blocks)):
-        logging.info("[%d]block : %s block_out : %d" % (i, blocks[i].toString(), blocks[i].getOutSize()))
- 
-    
+    '''
+    定位状态变量
+    '''
+    pcode = None
+    pcode_iterator = prologue_block.getIterator()
+    while pcode_iterator.hasNext():
+        pcode = pcode_iterator.next()
+        if pcode.getOpcode() == PcodeOp.COPY and pcode.getInput(0).isConstant():
+            logging.info("pcode : %s" % pcode.toString())
+
+    if not pcode:
+        logging.info("find varnode_pcode error!")
+        exit()
+
+    pcode_iterator = prologue_block.getIterator()
+    while pcode_iterator.hasNext():
+        pcode = pcode_iterator.next()
+        logging.info("pcode : %s" % pcode.toString())
+    # depth = 0
+    # while pcode is not None and pcode.getOpcode() != PcodeOp.MULTIEQUAL:
+    #     logging.debug('finding phi node: %s, depth %d' % (pcode, depth))
+    #     if pcode.getOutput() is None:
+    #         logging.warning('output is None in %s' % pcode)
+    #         break
+    #     pcode = pcode.getOutput().getLoneDescend()
+    #     if depth > 5:
+    #         break
+    #     depth += 1
+
+
+    '''
+    获取真实块
+    观察CFG，没有预处理器，将后继为主分发器的块都视为真实块
+    '''
+    # for i in range(2, len(blocks)):
+    #     logging.info("[%d]block : %s block_out : %d" % (i, blocks[i].toString(), blocks[i].getOutSize()))
+
     # for block in blocks:
     #     pcode =  get_last_pcode(block)
     #     if pcode.getOpcode() == PcodeOp.BRANCH:
@@ -73,10 +154,20 @@ if __name__ == '__main__':
     #             block_prev = block.getIn(0)
     #             logging.info(block)
     #             logging.info(block_prev)
-
+    #
     #             pcode_iterator = block_prev.getIterator()
     #             while pcode_iterator.hasNext():
     #                     pcode = pcode_iterator.next()
     #                     logging.info(pcode)
 
-
+    # for i in range(2, len(blocks)):
+    #     for j in range(blocks[i].getOutSize()):
+    #         if blocks[i].getOut(j) == main_dispatcher_block:
+    #             if blocks[i].getOutSize() > 1:
+    #                 pcode = get_last_pcode(blocks[i])
+    #                 logging.info("block : %s, pcode : %s" % (blocks[i].toString(), pcode))
+    #
+    #                 pcode_iterator = blocks[i].getIterator()
+    #                 while pcode_iterator.hasNext():
+    #                     pcode = pcode_iterator.next()
+    #                     logging.info(pcode)
